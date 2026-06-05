a77be95c9e716b881cb241ed16b915ccd07250e0
pujajathar <pujajathar91@.com>
2026-06-05 11:32:33 -0500

    commit in the feature-branch

---
+++ new_file.js
@@ -0,0 +1,42 @@
+function capatilize(string){
+    let newString = string.charAt(0).toUpperCase() + string.slice(1);
+    return newString;
+}console.log(capatilize("hello"));
+
+
+function capitalize(string){
+let newString = string.charAt(0).toUpperCase() + string.slice(1);
+return  newString;
+}console.log(capitalize("hello"));
+
+function lastLetter(string){
+  //for(let i = 0; i < string.lenght; i++)
+  
+return string.slice(-1);
+  
+}console.log(lastLetter("hello"));
+
+
+
+
+/*
+
+function capatilize(string){
+    let newString = string.charAt(0).toUpperCase() + string.slice(1);
+    return newString;
+}console.log(capatilize("hello"));
+
+
+function capitalize(string){
+let newString = string.charAt(0).toUpperCase() + string.slice(1);
+return  newString;
+}console.log(capitalize("hello"));
+
+function lastLetter(string){
+  //for(let i = 0; i < string.lenght; i++)
+  
+return string.slice(-1);
+  
+}console.log(lastLetter("hello"));
+
+*/
