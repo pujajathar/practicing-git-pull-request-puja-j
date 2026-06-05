907b5b5f44d5e3bb1b2744a95c8777706ede9150
pujajathar <pujajathar91@.com>
2026-06-05 11:29:52 -0500

    commit after main branch

---
+++ new.js
@@ -0,0 +1,17 @@
+function capatilize(string){
+    let newString = string.charAt(0).toUpperCase() + string.slice(1);
+    return newString;
+}console.log(capatilize("hello"));
+
+
+function capitalize(string){
+let newString = string.charAt(0).toUpperCase() + string.slice(1);
+return  newString;
+}console.log(capitalize("hello"));
+
+function lastLetter(string){
+  //for(let i = 0; i < string.lenght; i++)
+  
+return string.slice(-1);
+  
+}console.log(lastLetter("hello"));
